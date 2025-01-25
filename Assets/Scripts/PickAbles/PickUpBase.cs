6c569c69d6b3a80b28ae6770e9c0c44e94a9c818
Thalruk <Bartek.lipinski.pl@gmail.com>
2025-01-25 19:00:09 +0100

    asd

--- Assets/Scripts/PickAbles/PickUpBase.cs
+++ Assets/Scripts/PickAbles/PickUpBase.cs
@@ -2,16 +2,10 @@ using UnityEngine;
 
 public abstract class PickUpBase : MonoBehaviour
 {
-    float x = 0;
     [SerializeField] bool infinite;
     public abstract void PickUp();
-    private void Update()
-    {
-        x += Time.deltaTime;
-        transform.position = new Vector2(transform.position.x, transform.position.y + Mathf.Sin(x));
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
