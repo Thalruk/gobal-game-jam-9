0e29d01e02f5ba4778516441de1615169bbc1798
Pjoter9c <pduczek9c@wp.pl>
2025-01-25 19:57:42 +0100

    door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    
+    [SerializeField] Sprite openDoor;
+
+    Player player;
+    bool fade = false;
+    float timeFade = 0f;
+    Vector2 startPos;
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            startPos = Player.Instance.transform.position;
+            GetComponent<SpriteRenderer>().sprite = openDoor;
+            fade = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (fade)
+        {
+            Player.Instance.transform.localScale = Vector2.Lerp(new Vector2(1f, 1f), new Vector2(0.5f, 0.5f), timeFade);
+            Player.Instance.transform.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>().color = Color.Lerp(new Color(1f, 1f, 1f, 1f), new Color(1f, 1f, 1f, 0f), timeFade);
+            Player.Instance.transform.position = Vector2.Lerp(startPos, transform.position, timeFade);
+            timeFade += Time.deltaTime;
+        }
+    }
 }
