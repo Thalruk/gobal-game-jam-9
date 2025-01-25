fd0c80b29f255d53ef7273f0fc4eb3d5b199a4b3
Thalruk <Bartek.lipinski.pl@gmail.com>
2025-01-25 12:33:52 +0100

    asasDSASDASDASD

--- Assets/Scripts/Levels/Intro.cs
+++ Assets/Scripts/Levels/Intro.cs
@@ -7,26 +7,27 @@ public class Intro : MonoBehaviour
     [SerializeField] VideoPlayer vidPlayer;
     [SerializeField] GameObject text;
     [SerializeField] string nextSceneName;
-
+    float x = 0;
     private void Awake()
     {
-        vidPlayer.Prepare();
         Cursor.visible = false;
     }
 
     private void Update()
     {
-        if (vidPlayer.isPrepared)
-        {
-            vidPlayer.Play();
-        }
-
+        x += Time.deltaTime;
         if (Input.GetKeyUp(KeyCode.Space))
         {
             text.SetActive(true);
         }
 
         if (text.activeInHierarchy && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        print(x);
+        print(vidPlayer.length);
+        if (x >= vidPlayer.length)
         {
             SceneManager.LoadScene(nextSceneName);
         }
