a39500bf2694e5fff271f0edab1bc666165a6811
Thalruk <Bartek.lipinski.pl@gmail.com>
2025-01-25 20:36:11 +0100

    aa

--- Assets/Scripts/Levels/LevelManager.cs
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager Instance;
+    [SerializeField] public int score;
+    [SerializeField] int scoreMax;
+
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+
+        scoreMax = FindObjectsOfTypeAll(typeof(CollectiblePickUp)).Length;
     }
 }
